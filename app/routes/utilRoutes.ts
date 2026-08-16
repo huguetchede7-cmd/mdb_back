a35707ccfe733fd63296ca301eba05ac8a17cd4c
Sila1999 <huguetchede7@gmail.com>
2026-08-16 17:05:21 +0100

    chore: remove unused files from another project

--- app/routes/utilRoutes.ts
+++ app/routes/utilRoutes.ts
@@ -1,13 +1,6 @@
 import express from 'express'
-import { TestDemoController } from '../controllers/common/TestDemoController';
 
 const router = express.Router()
 
-
-//test email queue
-router.get("/test-email-send", TestDemoController.testEmailSend);
-
-//test email queue
-router.get("/test-google-sheet-reader", TestDemoController.getSheetByCode);
 
 export default router
